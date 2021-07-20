3f243a903809bb5395bf6d50a69ae8a8e3bffa47
morgofer174 <morgofer174@gmail.com>
2021-07-20 22:17:02 +0300

    starting DB

---
+++ src/test/java/Main/DB.java
@@ -0,0 +1,9 @@
+package Main;
+
+public class DB {
+    private static final String USER_NAME = "XXXXXXXXXXXXXXX";
+    private static final String DATABASE_NAME = "XXXXXXXXXXXXXX";
+    private static final String PASSWORD = "XXXXXXXXXXXXXX";
+    private static final String PORT = "3306";
+    private static final String SERVER = "remotemysql.com";
+}
